985ff998a5efab32b6142790245c713cd2c38170
albertbsnss0-lang <albertbsnss0@gmail.com>
2025-12-14 11:59:43 +0100

    Update widget.js

--- widget.js
+++ widget.js
@@ -295,15 +295,18 @@
         </div>
     `;
 
-    chatContainer.innerHTML = newConversationHTML + chatInterfaceHTML;
+const toggleButton = document.createElement('button');
+toggleButton.className = `chat-toggle${config.style.position === 'left' ? ' position-left' : ''}`;
 
-    const toggleButton = document.createElement('button');
-    toggleButton.className = `chat-toggle${config.style.position === 'left' ? ' position-left' : ''}`;
-    toggleButton.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M12 2C6.477 2 2 6.477 2 12c0 1.821.487 3.53 1.338 5L2.5 21.5l4.5-.838A9.955 9.955 0 0012 22c5.523 0 10-4.477 10-10S17.523 2 12 2z"/>
-        </svg>
-    `;
+toggleButton.innerHTML = `
+  <span class="launcher-badge">Online</span>
+
+  <svg class="launcher-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M12 2C6.477 2 2 6.477 2 12c0 1.821.487 3.53 1.338 5L2.5 21.5l4.5-.838A9.955 9.955 0 0012 22c5.523 0 10-4.477 10-10S17.523 2 12 2z"/>
+  </svg>
+
+  <span class="launcher-text">Get a Free Quote</span>
+`;
 
     widgetContainer.appendChild(chatContainer);
     widgetContainer.appendChild(toggleButton);
@@ -415,3 +418,4 @@
     );
 
 })();
+
